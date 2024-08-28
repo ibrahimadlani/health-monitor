cb926b658d1443f60a0ba4f6a21276cdffe576e4
Ibrahim ADLANI <ibrahim@adlani.com>
2024-08-28 21:17:25 +0200

    Adds django command for a polling the database.

---
+++ back/api/management/commands/wait_for_db.py
@@ -0,0 +1,23 @@
+import time
+import os
+from psycopg2 import OperationalError as Psycopg2OpError
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """Django command to wait for database."""
+
+    def handle(self, *args, **options):
+        """Entrypoint for command."""
+        self.stdout.write('Waiting for database...')
+        self.stdout.write(f"DB_USER: {os.environ.get('DB_USER')}, DB_PASSWORD: {os.environ.get('DB_PASSWORD')}, DB_NAME: {os.environ.get('DB_NAME')}, DB_HOST: {os.environ.get('DB_HOST')}, DB_PORT: {os.environ.get('DB_PORT')}")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2OpError, OperationalError) as e:
+                self.stdout.write(f'Database unavailable, waiting 1 second... {e}')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
