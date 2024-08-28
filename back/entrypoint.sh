b5223fbf16efbbfe03042f4983083a52a001d35d
Ibrahim ADLANI <ibrahim@adlani.com>
2024-08-28 21:31:38 +0200

    Adds a README.md

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 APP_PORT=${PORT:-8000}
 cd /app/
-/py/bin/gunicorn --worker-tmp-dir /dev/shm api.wsgi:application
+/py/bin/gunicorn --worker-tmp-dir /dev/shm core.wsgi:application
 --bind "0.0.0.0:${APP_PORT}"
