6d56b24dac9f30682a9f57b40629cdfa24e1d8da
Ibrahim ADLANI <ibrahim@adlani.com>
2024-08-28 20:14:01 +0200

    Sets the JWT enpoints.

--- back/api/urls.py
+++ back/api/urls.py
@@ -3,6 +3,12 @@ This file is used to define the urls of the core app.
 """
 
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+
+    # Auth
+    path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    
 ]
